12ab1c300f4e8185098b5b934867fceb7c32406f
Riju Vashisht <optimus4you@gmail.com>
2015-07-13 15:36:11 -0400

    Line Chart Working

---
+++ dist/appatronic-chart.js
@@ -0,0 +1,79 @@
+(function () {
+    'use strict';
+    angular.module('appatronic.chartjs', []);
+
+    angular.module('appatronic.chartjs')
+        .factory('chartjsservice', chartjsservice)
+        .directive('appatronicChart', ['chartjsservice', AppatronicChart]);
+
+    function AppatronicChart(chartjsservice) {
+        return {
+            restrict: 'EA',
+            scope: {
+                data: '=chartData',
+                options: '=chartOptions',
+                type: '@chartType',
+                legend: '=chartLegend',
+                chart: '=chart'
+            },
+            link: function ($scope, $element, $attrs) {
+                console.log($attrs.type);
+                var canvas = document.createElement('canvas');
+                document.body.appendChild(canvas);
+                var context = $element[0].getContext("2d");
+                var chart = chartjsservice.achart(context);
+                var appatronicChart;
+
+                $scope.$on('$destroy', function () {
+                    if (appatronicChart) {
+                        appatronicChart.destroy();
+                    }
+                });
+                $scope.$watch("data", function (value) {
+                    if (value) {
+                        if (appatronicChart) {
+                            appatronicChart.destroy();
+                        }
+                        if ($scope.type) {
+                            appatronicChart = chartjsservice.resolve(chart, $scope.type, $scope.data, $scope.options);
+                        }
+                        appatronicChart.resize();
+                    }
+                }, true);
+            }
+        }
+    }
+
+    function chartjsservice() {
+        var service = {
+            achart: function achart(context) {
+                return new Chart(context);
+            },
+            resolve: function resolve(achart, type, data, options) {
+                var chartType = GetChart(type);
+                if (chartType) {
+                    return achart[chartType](data, options);
+                } else {
+                    throw 'Please specify chart type in the attribute';
+                }
+
+                function GetChart(type) {
+                    switch (type) {
+                    case "line":
+                        return "Line";
+                        defualt:
+                            return;
+                    }
+                }
+
+            }
+        }
+        return service;
+
+
+
+
+
+
+    }
+})();
